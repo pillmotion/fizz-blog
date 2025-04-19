09e59a3ef7cf02f2e4335d4e4b30da481dd79015
huhongwei <381093478@qq.com>
2025-04-19 23:39:59 +0800

    wip

--- src/components/RelatedPosts.tsx
+++ src/components/RelatedPosts.tsx
@@ -16,7 +16,7 @@ export const RelatedPosts: FunctionComponent<{
   return (
     <div className="my-8">
       <div className="mb-6 text-lg font-semibold tracking-tight">
-        Related Posts
+        相关任务
       </div>
       <div className="grid grid-cols-1 gap-4 lg:grid-cols-3">
         {posts.slice(0, 3).map((post) => (
